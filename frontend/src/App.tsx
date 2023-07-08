8406f2e50696b3de7997cf5b9512dd09498caf23
ArpanaMishra94 <arpanamishra394@gmail.com>
2023-07-08 13:45:34 +0530

    Header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,17 @@
 import React from "react";
+import Header from "./components/Header";
+import { Container } from "react-bootstrap";
 
 function App() {
 	return (
-		<div className="App">
-			<header className="App-header">Welcome</header>
-		</div>
+		<>
+			<Header />
+			<main className="py-3">
+				<Container>
+					<h1>Welcome to Shopify</h1>
+				</Container>
+			</main>
+		</>
 	);
 }
 
