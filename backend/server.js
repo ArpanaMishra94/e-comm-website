4db7bdfbdcf645cf453354dacfc642a31cc4234e
ArpanaMishra94 <arpanamishra394@gmail.com>
2023-07-10 10:47:11 +0530

    backend init

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from 'dotenv';
+dotenv.config();
+import products from "./data/products.js";
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.get("/", (req, resp) => {
+	resp.send("API is running...");
+});
+
+app.get("/api/products", (req, resp) => {
+	resp.send(products);
+});
+
+app.get("/api/products/:id", (req, resp) => {
+	const product = products.find((p) => p._id === req.params.id);
+	resp.send(product);
+});
+
+app.listen(port, () => console.log("listening on port " + port));
