e27e4c929458dce00c6a3588b84af510ba1e2266
ArpanaMishra94 <arpanamishra394@gmail.com>
2023-07-18 17:35:46 +0530

    logout user and clear Cookie

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,105 +1,109 @@
 import asyncHandler from "../middleware/asyncHandler.js";
 import User from "../models/userModel.js";
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 // @desc  Auth user & get token
 // @route  GET/api/users/login
 // @access Public
 const authUser = asyncHandler(async (req, resp) => {
-    const {email, password} = req.body;
+	const { email, password } = req.body;
 
-    const user = await User.findOne({email});
+	const user = await User.findOne({ email });
 
-    if(user && (await user.matchPassword(password))) {
-        const token = jwt.sign({userId: user._id}, process.env.JWT_SECRET, {
-            expiresIn: '30d'
-        });
+	if (user && (await user.matchPassword(password))) {
+		const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+			expiresIn: "30d",
+		});
 
-        // Set JWT as HTTP-Only cookie
-        resp.cookie('jwt', token, {
-            httpOnly: true,
-            secure: process.env.NODE_ENV !== 'development',
-            sameSite: 'strict',
-            maxAge: 30 * 24 * 60 * 60 * 1000  // 30 Days
-        });
+		// Set JWT as HTTP-Only cookie
+		resp.cookie("jwt", token, {
+			httpOnly: true,
+			secure: process.env.NODE_ENV !== "development",
+			sameSite: "strict",
+			maxAge: 30 * 24 * 60 * 60 * 1000, // 30 Days
+		});
 
-        resp.json({
-            _id: user._id,
-            name: user.name,
-            email: user.email,
-            isAdmin: user.isAdmin
-        });
-    }
-    else { 
-        resp.status(401);
-        throw new Error('Invalid email or password');
-    }
+		resp.json({
+			_id: user._id,
+			name: user.name,
+			email: user.email,
+			isAdmin: user.isAdmin,
+		});
+	} else {
+		resp.status(401);
+		throw new Error("Invalid email or password");
+	}
 });
 
 // @desc  Register User
 // @route  POST/api/users
 // @access Public
 const registerUser = asyncHandler(async (req, resp) => {
-    resp.send('register user');
+	resp.send("register user");
 });
 
 // @desc  Logout user / clear cookie
 // @route  POST/api/users/logout
 // @access Private
 const logoutUser = asyncHandler(async (req, resp) => {
-    resp.send('logout user');
+	resp.cookie("jwt", "", {
+		httpOnly: true,
+		expires: new Date(0),
+	});
+
+	resp.status(200).json({ message: "Logged out successfully" });
 });
 
 // @desc  Get user profile
 // @route  GET/api/users/profile
 // @access Private
 const getUserProfile = asyncHandler(async (req, resp) => {
-    resp.send('get user profile');
+	resp.send("get user profile");
 });
 
 // @desc  Update user profile
 // @route  PUT/api/users/profile
 // @access Private
 const updateUserProfile = asyncHandler(async (req, resp) => {
-    resp.send('update user profile');
+	resp.send("update user profile");
 });
 
 // @desc  Get users
 // @route  GET/api/users
 // @access Private/Admin
 const getUsers = asyncHandler(async (req, resp) => {
-    resp.send('get users');
+	resp.send("get users");
 });
 
 // @desc  Get user by ID
 // @route  GET/api/users/:id
 // @access Private/Admin
 const getUserByID = asyncHandler(async (req, resp) => {
-    resp.send('get user by id');
+	resp.send("get user by id");
 });
 
 // @desc  Delete users
 // @route  DELETE/api/users/:id
 // @access Private/Admin
 const deleteUser = asyncHandler(async (req, resp) => {
-    resp.send('delete user');
+	resp.send("delete user");
 });
 
 // @desc  Update users
 // @route  PUT/api/users/:id
 // @access Private/Admin
 const updateUser = asyncHandler(async (req, resp) => {
-    resp.send('update user');
+	resp.send("update user");
 });
 
 export {
-    authUser,
-    registerUser,
-    logoutUser,
-    getUserProfile,
-    updateUserProfile,
-    getUsers,
-    deleteUser,
-    getUserByID,
-    updateUser,
-}
+	authUser,
+	registerUser,
+	logoutUser,
+	getUserProfile,
+	updateUserProfile,
+	getUsers,
+	deleteUser,
+	getUserByID,
+	updateUser,
+};
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,31 +1,34 @@
 import mongoose from "mongoose";
-import bcrypt from 'bcryptjs';
+import bcrypt from "bcryptjs";
 
-const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    password: {
-        type: String,
-        required: true,
-    },
-    isAdmin: {
-        type: Boolean,
-        required: true,
-        default: false,
-    },
-}, {
-    timeStamps: true,
-})
+const userSchema = new mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+		},
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		password: {
+			type: String,
+			required: true,
+		},
+		isAdmin: {
+			type: Boolean,
+			required: true,
+			default: false,
+		},
+	},
+	{
+		timeStamps: true,
+	}
+);
 
-userSchema.methods.matchPassword = async function(enteredPassword) {
-    return await bcrypt.compare(enteredPassword, this.password);
+userSchema.methods.matchPassword = async function (enteredPassword) {
+	return await bcrypt.compare(enteredPassword, this.password);
 };
 
 const User = mongoose.model("User", userSchema);
