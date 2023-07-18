3288ecfc29ee16bd00e34ab5b81daa0a120eb848
ArpanaMishra94 <arpanamishra394@gmail.com>
2023-07-18 23:13:13 +0530

    Auth and User API Slice

---
+++ frontend/src/slices/authSlice.ts
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialUserInfo = localStorage.getItem("userInfo");
+const initialState = {
+	userInfo: initialUserInfo ? JSON.parse(initialUserInfo) : null,
+};
+
+const authSlice = createSlice({
+	name: "auth",
+	initialState,
+	reducers: {
+		setCredentials: (state, action) => {
+			state.userInfo = action.payload;
+			localStorage.setItem("userInfo", JSON.stringify(action.payload));
+		},
+	},
+});
+
+export const { setCredentials } = authSlice.actions;
+
+export default authSlice.reducer;
