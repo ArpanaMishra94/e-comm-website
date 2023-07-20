97d4f494f06192c2395a99922eca0de4c9703736
ArpanaMishra94 <arpanamishra394@gmail.com>
2023-07-20 15:20:54 +0530

    checkout steps component

--- frontend/src/screens/ShippingScreen.tsx
+++ frontend/src/screens/ShippingScreen.tsx
@@ -4,6 +4,7 @@ import { Form, Button } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { saveShippingAddress } from "../slices/cartSlice";
+import CheckoutSteps from "../components/CheckoutSteps";
 
 const ShippingScreen = () => {
 	const cart = useSelector((state: any) => state.cart);
@@ -27,6 +28,7 @@ const ShippingScreen = () => {
 
 	return (
 		<FormContainer>
+			<CheckoutSteps step1 step2 step3={false} step4={false} />
 			<h1>Shipping</h1>
 
 			<Form onSubmit={submitHandler}>
