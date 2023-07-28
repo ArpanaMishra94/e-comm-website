2f7628dd8f9c548c9865711a5bb7124ced4ebed4
ArpanaMishra94 <arpanamishra394@gmail.com>
2023-07-28 12:23:34 +0530

    backend user routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -121,28 +121,66 @@ const updateUserProfile = asyncHandler(async (req, resp) => {
 // @route  GET/api/users
 // @access Private/Admin
 const getUsers = asyncHandler(async (req, resp) => {
-	resp.send("get users");
+	const users = await User.find({});
+	resp.status(200).json(users);
 });
 
 // @desc  Get user by ID
 // @route  GET/api/users/:id
 // @access Private/Admin
 const getUserByID = asyncHandler(async (req, resp) => {
-	resp.send("get user by id");
+	const user = await User.findById(req.params.id).select("-password");
+
+	if (user) {
+		resp.status(200).json(user);
+	} else {
+		resp.status(404);
+		throw new Error("User not found");
+	}
 });
 
 // @desc  Delete users
 // @route  DELETE/api/users/:id
 // @access Private/Admin
 const deleteUser = asyncHandler(async (req, resp) => {
-	resp.send("delete user");
+	const user = await User.findById(req.params.id);
+
+	if (user) {
+		if (user.isAdmin) {
+			resp.status(400);
+			throw new Error("Cannot delete admin user");
+		}
+		await User.deleteOne({ _id: user._id });
+		resp.status(200).json({ message: "User deleted successfully" });
+	} else {
+		resp.status(404);
+		throw new Error("User not found");
+	}
 });
 
 // @desc  Update users
 // @route  PUT/api/users/:id
 // @access Private/Admin
 const updateUser = asyncHandler(async (req, resp) => {
-	resp.send("update user");
+	const user = await User.findById(req.params.id);
+
+	if (user) {
+		user.name = req.body.name || user.name;
+		user.email = req.body.email || user.email;
+		user.isAdmin = Boolean(req.body.isAdmin);
+
+		const updatedUser = await user.save();
+
+		resp.status(200).json({
+			_id: updatedUser._id,
+			name: updatedUser.name,
+			email: updatedUser.email,
+			isAdmin: updatedUser.isAdmin,
+		});
+	} else {
+		resp.status(404);
+		throw new Error("User not found");
+	}
 });
 
 export {
